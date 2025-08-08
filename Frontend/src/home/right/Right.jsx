4d006bfbbb15da4d0a64499928dfe3fc2abeb8e3
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-08 12:18:35 +0530

    left right partition

--- Frontend/src/home/right/Right.jsx
+++ Frontend/src/home/right/Right.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Right = () => {
   return (
-     <div className="w-[70%] border border-white bg-slate-950 text-white">
+     <div className="w-[70%]  bg-slate-950 text-white">
       Right
     </div>
   )
