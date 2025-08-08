73ba28cf57d92037799a8ce8bf123aa04b3b0654
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-08 13:04:32 +0530

    handling wih users

--- Frontend/src/home/left/Users.jsx
+++ Frontend/src/home/left/Users.jsx
@@ -1,130 +1,19 @@
 import React from 'react'
+import User from './User'
 
 const Users = () => {
   return (
-    <div>
-    <div className='flex space-x-4 px-6 py-7 hover:bg-slate-600 duration-300 cursor-pointer'>
-      <div className='avatar online'>
-        <div className='w-14 rounded-full'>
-            <img src='https://avatars.githubusercontent.com/u/113823362?s=400&u=30a64db39b6ac2210c229df31ad85b959e2a3bb4&v=4' />
-            
-        </div>
-      </div>
-      <div>
-        <h1>Ankita Arya</h1>
-        <span>ankitaarya12@gmail.com</span>
-      </div>
-       <div className='avatar online'>
-        <div className='w-14 rounded-full'>
-            <img src='https://avatars.githubusercontent.com/u/113823362?s=400&u=30a64db39b6ac2210c229df31ad85b959e2a3bb4&v=4' />
-            
-        </div>
-      </div>
-      <div>
-        <h1>Ankita Arya</h1>
-        <span>ankitaarya12@gmail.com</span>
-      </div>
-       <div className='avatar online'>
-        <div className='w-14 rounded-full'>
-            <img src='https://avatars.githubusercontent.com/u/113823362?s=400&u=30a64db39b6ac2210c229df31ad85b959e2a3bb4&v=4' />
-            
-        </div>
-      </div>
-      <div>
-        <h1>Ankita Arya</h1>
-        <span>ankitaarya12@gmail.com</span>
-      </div>
-       <div className='avatar online'>
-        <div className='w-14 rounded-full'>
-            <img src='https://avatars.githubusercontent.com/u/113823362?s=400&u=30a64db39b6ac2210c229df31ad85b959e2a3bb4&v=4' />
-            
-        </div>
-      </div>
-      <div>
-        <h1>Ankita Arya</h1>
-        <span>ankitaarya12@gmail.com</span>
-      </div>
-       <div className='avatar online'>
-        <div className='w-14 rounded-full'>
-            <img src='https://avatars.githubusercontent.com/u/113823362?s=400&u=30a64db39b6ac2210c229df31ad85b959e2a3bb4&v=4' />
-            
-        </div>
-      </div>
-      <div>
-        <h1>Ankita Arya</h1>
-        <span>ankitaarya12@gmail.com</span>
-      </div>
-       <div className='avatar online'>
-        <div className='w-14 rounded-full'>
-            <img src='https://avatars.githubusercontent.com/u/113823362?s=400&u=30a64db39b6ac2210c229df31ad85b959e2a3bb4&v=4' />
-            
-        </div>
-      </div>
-      <div>
-        <h1>Ankita Arya</h1>
-        <span>ankitaarya12@gmail.com</span>
-      </div>
-       <div className='avatar online'>
-        <div className='w-14 rounded-full'>
-            <img src='https://avatars.githubusercontent.com/u/113823362?s=400&u=30a64db39b6ac2210c229df31ad85b959e2a3bb4&v=4' />
-            
-        </div>
-      </div>
-      <div>
-        <h1>Ankita Arya</h1>
-        <span>ankitaarya12@gmail.com</span>
-      </div>
-       <div className='avatar online'>
-        <div className='w-14 rounded-full'>
-            <img src='https://avatars.githubusercontent.com/u/113823362?s=400&u=30a64db39b6ac2210c229df31ad85b959e2a3bb4&v=4' />
-            
-        </div>
-      </div>
-      <div>
-        <h1>Ankita Arya</h1>
-        <span>ankitaarya12@gmail.com</span>
-      </div>
-       <div className='avatar online'>
-        <div className='w-14 rounded-full'>
-            <img src='https://avatars.githubusercontent.com/u/113823362?s=400&u=30a64db39b6ac2210c229df31ad85b959e2a3bb4&v=4' />
-            
-        </div>
-      </div>
-      <div>
-        <h1>Ankita Arya</h1>
-        <span>ankitaarya12@gmail.com</span>
-      </div>
-       <div className='avatar online'>
-        <div className='w-14 rounded-full'>
-            <img src='https://avatars.githubusercontent.com/u/113823362?s=400&u=30a64db39b6ac2210c229df31ad85b959e2a3bb4&v=4' />
-            
-        </div>
-      </div>
-      <div>
-        <h1>Ankita Arya</h1>
-        <span>ankitaarya12@gmail.com</span>
-      </div>
-       <div className='avatar online'>
-        <div className='w-14 rounded-full'>
-            <img src='https://avatars.githubusercontent.com/u/113823362?s=400&u=30a64db39b6ac2210c229df31ad85b959e2a3bb4&v=4' />
-            
-        </div>
-      </div>
-      <div>
-        <h1>Ankita Arya</h1>
-        <span>ankitaarya12@gmail.com</span>
-      </div>
-       <div className='avatar online'>
-        <div className='w-14 rounded-full'>
-            <img src='https://avatars.githubusercontent.com/u/113823362?s=400&u=30a64db39b6ac2210c229df31ad85b959e2a3bb4&v=4' />
-            
-        </div>
-      </div>
-      <div>
-        <h1>Ankita Arya</h1>
-        <span>ankitaarya12@gmail.com</span>
-        </div>
-      </div>
+    <div style={{maxHeight:"calc(92vh)"}} className='overflow-y-auto'>
+        <User></User>
+        <User></User>
+        <User></User>
+        <User></User>
+        <User></User>
+        <User></User>
+        <User></User>
+        <User></User>
+        <User></User>
+        <User></User>
     </div>
     
   )
