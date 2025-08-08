ea6094b3166ff66422e341257026a8a8407b4904
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-08 12:33:56 +0530

    handling with left partition

--- Frontend/src/home/left/Left.jsx
+++ Frontend/src/home/left/Left.jsx
@@ -5,7 +5,7 @@ const Left = () => {
   return (
     <div className="w-[30%]  bg-black text-gray-300">
 
-      <h1 className='font-bold'>Chat</h1>
+     <h1 className="font-bold text-3xl p-2 px-11">Chats</h1>
       <Search/>
     </div>
   )
