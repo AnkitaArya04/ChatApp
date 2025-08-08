48e8e893d451ac587464adfae94b4d92be9db7ad
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-08 18:37:15 +0530

    handling with search cion

--- Frontend/src/home/left1/Logout.jsx
+++ Frontend/src/home/left1/Logout.jsx
@@ -6,7 +6,7 @@ const Logout = () => {
       <div className="w-[4%]   bg-slate-950 text-white  flex flex-col justify-end ">
         <div className="p-3  align-bottom ">
           <button>
-            <IoSearch className='text-5xl p-2 hover:bg-gray-600 ounded-full duration-full' />
+            <IoSearch className='text-5xl p-2 hover:bg-gray-600 rounded-full duration-full' />
           </button>
         </div>
       </div>
