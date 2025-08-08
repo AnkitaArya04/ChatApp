37a5ba58cc785fab0dc21822ce50eccd8b359cb6
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-08 12:44:47 +0530

    handling wih users

--- Frontend/src/home/left/Left.jsx
+++ Frontend/src/home/left/Left.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Search from './Search'
+import Users from './Users'
 
 const Left = () => {
   return (
@@ -7,6 +8,7 @@ const Left = () => {
 
      <h1 className="font-bold text-3xl p-2 px-11">Chats</h1>
       <Search/>
+      <Users/>
     </div>
   )
 }
--- Frontend/src/home/left/Users.jsx
+++ Frontend/src/home/left/Users.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Users = () => {
   return (
     <div>
-      Users
+      <div className='avatar online'>
+        <div className='w-24 rounded-full'>
+            <img src='https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp' />
+            
+        </div>
+      </div>
     </div>
   )
 }
