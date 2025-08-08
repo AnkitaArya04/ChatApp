23b92c27346cb16ed8a7bbaa0659ec3740535d18
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-08 18:30:17 +0530

    handling wih users

--- Frontend/src/home/left1/Logout.jsx
+++ Frontend/src/home/left1/Logout.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import {IoSearch} from "react-icons/io5"
 const Logout = () => {
   return (
-     <div className="w-[4%]  bg-slate-950 text-white">
-      Logout
-    </div>
+     <>
+      <div className="w-[4%]   bg-slate-950 text-white  flex flex-col justify-end ">
+        <div className="p-3  align-bottom ">
+          <button>
+            <IoSearch className='text-5xl p-2 hover:bg-gray-600 ounded-full duration-full' />
+          </button>
+        </div>
+      </div>
+    </>
   )
 }
 
