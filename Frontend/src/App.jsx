e29c8390e0bcb3f58ed1ef96792c0c7037b4a7c0
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-12 18:46:25 +0530

    chatApp is initialized

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Left from './home/left/Left'
+import Right from './home/right/Right'
 
 const App = () => {
   return (
     <div>
       App
+      <Left/>
+      <Right/>
     </div>
   )
 }
