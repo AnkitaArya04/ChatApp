1ece1cbd6b1f8f587c987e9b33df695e214d5251
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-12 23:27:21 +0530

    tailwind css set up

--- Frontend/src/home/left/Search.jsx
+++ Frontend/src/home/left/Search.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import {IoSearch} from "react-icons/io5"
 const Search = () => {
   return (
     <div>
-      Search
+     <form action="">
+        <label className="input input-bordered flex items-center gap-2">
+
+        </label>
+        <button>
+            <IoSearch/>
+        </button>
+     </form>
     </div>
   )
 }
