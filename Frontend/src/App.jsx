e36db021b931f1174ea0bf1b4c77a9b93916f3ca
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-13 22:42:09 +0530

    handling with left

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,62 @@
-import React from 'react'
-import Left from './home/left/Left'
-import Right from './home/right/Right'
+import React from "react";
+import Left from "./home/Leftpart/Left";
+import Right from "./home/Rightpart/Right";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import { useAuth } from "./context/AuthProvider";
+import { Toaster } from "react-hot-toast";
+import Logout from "./home/left1/Logout";
 
-
-const App = () => {
+import { Navigate, Route, Routes } from "react-router-dom";
+function App() {
+  const [authUser, setAuthUser] = useAuth();
+  console.log(authUser);
   return (
-    <div>
-       <div className="flex h-screen">
-        <Left/>
-        <Right/>
-      </div>
-    </div>
-  )
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            authUser ? (
+              <div className="flex h-screen">
+                <Logout />
+                <Left />
+                <Right />
+              </div>
+
+
+            
+            ) : (
+              <Navigate to={"/login"} />
+            )
+          }
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <Signup />}
+        />
+      </Routes>
+      <Toaster />
+    </>
+  );
 }
 
-export default App
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
