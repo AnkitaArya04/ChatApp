5b2930a2eb4f19500d2cb759be3a66b8aa766905
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-13 22:25:11 +0530

    tailwind css set up

--- Frontend/src/home/left/Left.jsx
+++ Frontend/src/home/left/Left.jsx
@@ -2,8 +2,9 @@ import React from "react"
 import Search from "./Search"
 const Left = () => {
   return (
-    <div className="w-[30%] bg-black text-white ">
-      
+    <div className="w-[30%] bg-black text-gray-300">
+       <h1 className="font-bold text-3xl p-2 px-11">Chats</h1>
+    {/* <div className="w-full   bg-black text-gray-300"> */}
       <Search/>
     </div>
   )
