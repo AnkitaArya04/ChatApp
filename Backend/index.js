0f4f0dbd6672eaddc722b464143af7892f356671
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-14 15:23:00 +0530

    dealng with backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,13 @@
-const express = require('express')
+import express from "express";
+import dotenv from "dotenv";
 const app=express()
-const port = 5002
+
+const PORT = process.env.PORT || 5000;
 
 app.get('/',(req,res)=>{
     res.send('Hello World!')
 })
 
 app.listen(port, ()=>{
-    console.log(`Example app listening on port ${port}`)
+    console.log(`server is running on port ${port}`)
 })
