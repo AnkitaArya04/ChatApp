47c1c0226a5d0f61c7d1f68aa562980751e260fc
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-14 15:44:56 +0530

    bulding database

--- Backend/index.js
+++ Backend/index.js
@@ -2,8 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 const app=express()
-
+dotenv.config();
 const PORT = process.env.PORT || 5001;
+const URI = process.env.MONGODB_URI;
+
+try {
+    mongoose.connect(URI);
+    console.log("Connected to MongoDB");
+} catch (error) {
+    console.log(error);
+}
 
 app.get('/',(req,res)=>{
     res.send('Hello World!')
