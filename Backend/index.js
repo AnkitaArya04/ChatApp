9fa881f2fcb4b56b68ef7ec956f9a5ca4ac80842
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-14 15:33:13 +0530

    bulding database

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 const app=express()
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.get('/',(req,res)=>{
     res.send('Hello World!')
 })
 
-app.listen(port, ()=>{
-    console.log(`server is running on port ${port}`)
+app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`)
 })
