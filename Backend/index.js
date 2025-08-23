d4cb9b8fa583acccfd473ead80285ef3b678c102
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-08-23 18:40:37 +0530

    chatapp

--- Backend/index.js
+++ Backend/index.js
@@ -1,22 +1,34 @@
-import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-const app=express()
-dotenv.config();
-const PORT = process.env.PORT || 5001;
-const URI = process.env.MONGODB_URI;
+// import express from "express";
+// import dotenv from "dotenv";
+// import mongoose from "mongoose";
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
 
-try {
-    mongoose.connect(URI);
-    console.log("Connected to MongoDB");
-} catch (error) {
-    console.log(error);
-}
+// import userRoute from "./routes/user.route.js";
+// import messageRoute from "./routes/message.route.js";
+// import { app, server } from "./SocketIO/server.js";
 
-app.get('/',(req,res)=>{
-    res.send('Hello World!')
-})
+// dotenv.config();
 
-app.listen(PORT, ()=>{
-    console.log(`server is running on port ${PORT}`)
-})
+// // middleware
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(cors());
+
+// const PORT = process.env.PORT || 3001;
+// const URI = process.env.MONGODB_URI;
+
+// try {
+//     mongoose.connect(URI);
+//     console.log("Connected to MongoDB");
+// } catch (error) {
+//     console.log(error);
+// }
+
+// //routes
+// app.use("/api/user", userRoute);
+// app.use("/api/message", messageRoute);
+
+// server.listen(PORT, () => {
+//     console.log(`Server is Running on port ${PORT}`);
+// });
